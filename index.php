e92dbe73dd62fcafa9039f3eea5c6fc7e4883d14
DESKTOP-9VIPES5\20171025 <128yichu128@gmail.com>
2020-11-05 20:50:37 +0800

    首頁增加ajax.php

--- index.php
+++ index.php
@@ -13,17 +13,20 @@ try{
 //     echo JSON_encode($result);
 //     print_r($data);
 
-}catch(PDOException $e){
-  $Errmsg.= '錯誤內容：' . $e->getMessage() . '<br>';
-  $Errmsg.= '錯誤行數：' . $e->getLine() . '<br>';
-}
-    echo $Errmsg;
 if($data->rowCount()==0){
   echo '資料有誤';
 }else{
   $result = $data->fetchAll(PDO::FETCH_ASSOC);
   echo JSON_encode($result);
 }
+
+
+}catch(PDOException $e){
+  $Errmsg.= '錯誤內容：' . $e->getMessage() . '<br>';
+  $Errmsg.= '錯誤行數：' . $e->getLine() . '<br>';
+  echo $Errmsg;
+}
+    
 
 ?>
 
