48808f6d96dbca2618cfa541e39f81976cb46726
nayu <nayuswork@gmail.com>
2020-11-17 19:29:45 +0800

    QAQ

--- connectbook.php
+++ connectbook.php
@@ -1,12 +1,12 @@
-<?php
-$dsn='mysql:
-      host=localhost;
-      port=3306;
-      dbname=ed103g4;
-      charset=utf8;';
-$uesr='ed103g4';
-$pass='ed103g4';
-$options=array(PDO::ATTR_CASE=>PDO::CASE_NATURAL,
-               PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION);
-$pdo = new PDO($dsn,$uesr,$pass,$options);
-?>
+<?php 
+$dsn = "mysql:
+    host=localhost;
+    port=3306;
+    dbname=ed103g4;
+    charset=utf8";
+$user = "root";
+$password = "nayusosad";
+$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                 PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+$pdo = new PDO($dsn, $user, $password, $options);
+?>   
