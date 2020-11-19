e6f246d526835db181124702a7c88e17582d2d32
noranuan <weipny912@gmail.com>
2020-11-19 14:20:01 +0800

    文章檢舉接php

---
+++ back_art_rep.php
@@ -0,0 +1,32 @@
+<?php
+$Errmsg = '';
+
+try{
+    // require_once('connectBooks.php'); //換成自己的 php $pdo來源
+    require_once('connectbook.php'); //換成自己的 php $pdo來源
+    $sql = 'SELECT 
+                A.ARTICLE_RE_NO ,
+                S.ARTICLE_NO,
+                A.ART_REPORT_REASON,
+                A.ART_REPORT_TIME,
+                A.ART_REPORT_STATUS
+            FROM article_report A
+	            JOIN article_sharing S on( A.ARTICLE_NO = S.ARTICLE_NO);';
+
+    $data = $pdo->prepare($sql);
+    $data-> execute();
+
+    if($data->rowCount()==0){
+        echo '資料有誤'; //資料庫沒有對應的資料
+    }else{
+        $result = $data->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($result);
+    }
+
+}catch(PDOException $e){
+    $Errmsg.= $e->getLine() . '<br>' . $e->getMessage();
+    echo $Errmsg;
+}
+
+
+?>
