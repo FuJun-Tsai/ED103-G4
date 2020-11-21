edc3d2996dc1a1f92b0c45528b9d4fec2ef80ab2
FuJun-Tsai <gdfvsgfd8769@gmail.com>
2020-11-21 16:53:17 +0800

    123

--- searchRenderLM.php
+++ searchRenderLM.php
@@ -13,6 +13,7 @@ for($i=0;$i<count($e);$i+=1){
 try{
     require_once('./connectbook.php');
     $sql = "SELECT 
+                R.RES_MESSAGE_NO,
                 concat('R' , R.RES_NO) as resno,
                 concat('L' , R.RES_MESSAGE_NO) as id,
                 DATE_FORMAT(R.RES_MES_TIME,'%Y-%m-%d %H:%i') as time,
@@ -20,15 +21,18 @@ try{
                 mm.MEMBER_IMAGE as mmimg
 
             FROM restaurant_message R
-                JOIN member_management mm on(R.MEMBER_NO = mm.MEMBER_NO)    
+                JOIN member_management mm on(R.MEMBER_NO = mm.MEMBER_NO)  
+                LEFT JOIN report_restaurant_message rrm on(R.RES_MESSAGE_NO = rrm.MESSAGE_NO)  
 
-            WHERE RES_NO in($word)
+            WHERE 
+                RES_NO in($word) AND
+                rrm.MESSAGE_NO in(0,2)
 
             ORDER BY RES_NO;";
   
     $RESdata = $pdo->prepare($sql);
     $RESdata-> execute();
-
+    echo $sql;
     if($RESdata->rowCount()==0){
         echo '資料有誤';
     }else{
